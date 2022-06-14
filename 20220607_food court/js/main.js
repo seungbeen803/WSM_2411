436ee3a1fd87f701de26450dfe45950e55b61ce1
Seungbeen <seungbin803@gmail.com>
2022-06-14 14:59:27 +0900

    js 여러가지 함수 모습 복습

---
+++ 20220607_food court/js/main.js
@@ -0,0 +1,13 @@
+/* 함수 선언식 */
+// function say() {
+//     console.log('hello world');
+// }
+
+/* 함수 표현식 */
+// const say = function() {
+//     console.log('hello world2');
+// }
+
+/* 화살표 함수 */
+// const say = () => console.log('hello world3');
+// say();
